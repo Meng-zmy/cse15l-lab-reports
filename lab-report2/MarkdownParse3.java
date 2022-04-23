45a5aaa9fe4ae5fa9336277436b9ccddb804d3c3
Meng-zmy <mez001@ucsd.edu>
2022-04-23 11:26:41 -0700

    4

--- lab-report2/MarkdownParse3.java
+++ lab-report2/MarkdownParse3.java
@@ -16,7 +16,11 @@ public class MarkdownParse3 {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+            if (!markdown.substring(openParen + 1, closeParen).contains(" ")) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+
             currentIndex = closeParen + 1;
         }
 
